813a6890c691cbfa6589570c2030471a5a738e2b
Facundo1508 <facundocontreras2012@gmail.com>
2021-04-02 00:20:16 -0300

    no me la conteiner

--- ProductsVnumToWoo.php
+++ ProductsVnumToWoo.php
@@ -22,7 +22,7 @@ $woocommerce = new Client(
 // Conexión API VNVM. Esto tenemos que postear 
 // ===========================================
 
-$url_API = "80.35.251.17/cgi-vel/vnvm/api.pro?w_as=5684|ART_BUS|GET|100|1|1|1|Publicable|555|555";
+$url_API = "80.35.251.17/cgi-vel/vnvm/api.pro?w_as=5684|ART_BUS|GET|100|1|1|1|Publicable|320|320";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -35,75 +35,114 @@ $items_origin = curl_exec($ch);
 
 curl_close($ch);
 
+
+if (!$items_origin) {
+    exit('❗Error en API origen');
+}
+
+$getDecodedVnvm = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $items_origin));
+
+
+//Este es el Objeto que trae Vnvm, sacamos el Id para mandarlo al insert como sku , y para comparar que no haya otro igual 
+$datosClientes = (object)$getDecodedVnvm->articulos;
+
+$registros = $datosClientes->registros;
+
+$sku = $registros[0]->id;
+
+//Este es el objeto que trae Woocommerce, por el sku. Si existe el objeto termina la ejecucion 
+$params = [
+    'sku' => (string)$sku
+];
+
+$getSku = $woocommerce->get('products', $params);
+
+
+if ($getSku) {
     
-    if (!$items_origin) {
-        exit('❗Error en API origen');
-    }
-    
-    $getDecodedVnvm = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $items_origin));
-    
-    
-    //Este es el Objeto que trae Vnvm, sacamos el Id para mandarlo al insert como sku , y para comparar que no haya otro igual 
-    $datosClientes = (object)$getDecodedVnvm->articulos;
+    $idUpdate = $getSku[0]->sku;
+    exit('❗Ya existe el producto, sku = ' . $sku);
+
+} else {
     
     $registros = $datosClientes->registros;
-    
-    $sku = $registros[0]->id;
-    
-    //Este es el objeto que trae Woocommerce, por el sku. Si existe el objeto termina la ejecucion 
-    $params = [
-        'sku' => (string)$sku
-    ];
-    
-    $getSku = $woocommerce->get('products', $params);
-    
-    
-    if ($getSku) {
-        
-        $idUpdate = $getSku[0]->sku;
-        exit('❗Ya existe el producto, sku = '.$sku);
-    
-    } else {
-    
-    
-        foreach ($datosClientes->registros as $key => $value) {
-    
-            $data = [
-                'name' => $value->nombre,
-                'type' =>  "simple",
-                'regular_price' => '212',
-                'description' => $value->metaDescripcion,
-                'short_description' => 'asdasdasd',
-                'sku' => (string)$sku,
-                'categories' => [
-                    [
-                        'id' => 9
-                    ],
-                    [
-                        'id' => 14
-                    ]
-                ],
-                'images' => [
-                    [
-                        'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
-                    ],
-                    [
-                        'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
-                    ]
-                ]
-            ];
-        }
-    
-        $resultCreate = $woocommerce->post('products',  $data);
-    
-        if (!$resultCreate) {
-            echo ("❗Error al actualizar productos \n");
-        } else {
-            print("✔ Productos actualizados correctamente \n <br>");
-            print_r($resultCreate);
-        }
+    // print_r($registros[0]);
+    // die;
+
+    $imgVisd = $registros[0]->imagenes;
+    // print_r($imgVisd[0]->visd);
+    // die;
+
+    foreach ($imgVisd as $key => $value) {
+
+        $images[$key] = [ 
+            'src' => (string)'http://80.35.251.17/cgi-vel/pruebas/'.$value->visd,
+        ];
     }
-    
+
+    // print_r($images);
+    // die;
+
+    $data = [
+
+        'name' => $registros[0]->nombre,
+        'type' =>  'simple',
+        'price' => '',
+        'regular_price' => '212',
+        'description' => $registros[0]->metaDescripcion,
+        'short_description' => 'asdasdasd',
+        'sku' => (string)$sku,
+
+        // 'dimensions' => [
+
+        //     'length' => "",
+        //     'width' =>"",
+        //     'height' => ""
+        // ],
+
+        // 'stock_quantity' => null,
+        // 'stock_status' => '',
+        // 'catalog_visibility' => '',
+        // //oferta
+        // 'sale_price' => '',
+        // //novedad
+        // 'featured' => null,
+        // //promoción
+        // 'date_on_sale_from' => null,
+        // 'date_on_sale_to' => null,
 
 
+        'categories' => [
+            [
+                'id' => 9
+            ],
+            [
+                'id' => 14
+            ]
+        ],
 
+        'images' => $images,
+
+        // 'images' => [
+        //     [
+        //         'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
+        //     ]
+        // ]
+        // 'downloads' => [
+
+        // ],
+
+    ];
+
+    // print_r($data);
+    // die;
+
+    $resultCreate = $woocommerce->post('products',  $data);
+
+    if (!$resultCreate) {
+        echo ("❗Error al actualizar productos \n");
+    } else {
+        print("✔ Productos actualizados correctamente \n <br>");
+        print_r($resultCreate);
+    }
+}
