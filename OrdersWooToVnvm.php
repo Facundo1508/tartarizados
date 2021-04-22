bf7a711993fa1dfab1c3d3b5c826b7962092c02f
Facundo1508 <facundocontreras2012@gmail.com>
2021-04-21 23:47:24 -0300

    asdsada

--- OrdersWooToVnvm.php
+++ OrdersWooToVnvm.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pedidos Woocommerce a Vnvm</title>
+</head>
+<body>
 <?php
 
 header('Content-type: text/html; charset=utf-8');
@@ -135,3 +144,6 @@ $objOrderWoo = (object)$getPedidosWoo;
         curl_close($ch);
 
 ?>
+    
+</body>
+</html>
