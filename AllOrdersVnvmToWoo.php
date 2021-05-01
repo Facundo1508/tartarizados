cc2d7e117da091fdb84bea7ce879e68b8633b13e
Facundo1508 <facundocontreras2012@gmail.com>
2021-04-30 21:42:00 -0300

    asdasdsa

--- AllOrdersVnvmToWoo.php
+++ AllOrdersVnvmToWoo.php
@@ -22,13 +22,13 @@ $woocommerce = new Client(
 );
 // Conexión API VNVM. Esto tenemos que postear 
 // ===========================================
-$desde = (String)$_POST['desde'];
+$desde = (string)$_POST['desde'];
 $newDesde = date("d-m-Y", strtotime($desde));
 
 $hasta = $_POST['hasta'];
 $newHasta = date("d-m-Y", strtotime($hasta));
 
-$url_API = "80.35.251.17/cgi-vel/pruebas/api.pro?w_as=5684|PV_BUS|GET|500|1|0|0||".$newDesde."|".$newHasta;
+$url_API = "80.35.251.17/cgi-vel/pruebas/api.pro?w_as=5684|PV_BUS|GET|500|1|0|0||" . $newDesde . "|" . $newHasta;
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -39,119 +39,92 @@ echo "➜ Obteniendo datos origen Vnvm ... \n";
 
 $items_origin = curl_exec($ch);
 
-
 curl_close($ch);
 
-    
-    if (!$items_origin) {
-        exit('❗Error en API origen');
-    }
 
-    $getDecodedVnvm = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $items_origin));
+if (!$items_origin) {
+    exit('❗Error en API origen');
+}
 
-    //Este es el Objeto que trae Vnvm, sacamos el Id para mandarlo al insert como sku , y para comparar que no haya otro igual 
-    $datosClientes = (object)$getDecodedVnvm->articulos;
-    
-    $registros = $datosClientes->registros;
-    if( $datosClientes->totalRegistros <= 0 ){
-        echo("No se encontraron registros con el Id proporcionado.");
-        exit();
-    }
-    
-    $int=0;
-    foreach($registros as $registros) {
+$getDecodedVnvm = json_decode(preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $items_origin));
 
-        $sku = $registros->id;
-        //Este es el objeto que trae Woocommerce, por el sku. Si existe el objeto termina la ejecucion 
-        $params = [
-            'sku' => (string)$sku
+//Este es el Objeto que trae Vnvm, sacamos el Id para mandarlo al insert como sku , y para comparar que no haya otro igual 
+$datosClientes = (object)$getDecodedVnvm->pedidos;
+
+
+$registros = $datosClientes->registros;
+
+if ($datosClientes->totalRegistros <= 0) {
+    echo ("No se encontraron registros con el Id proporcionado.");
+    exit();
+}
+
+$int = 0;
+foreach ($registros as $registros) {
+
+    $obj = $registros->direccionEnvio;
+    $objDetPed = $registros->detallePedido;
+
+    foreach ($objDetPed as $key => $value) {
+                
+        $line_items[$key] = [ 
+            'sku' =>  $value->refArticulo,
+            'quantity' => $value->cantPedida
         ];
-        
-        $getSku = $woocommerce->get('products', $params);
-        
-        
-        if ($getSku) {
-            
-            $idUpdate = $getSku[0]->sku;
-            echo('❗Ya existe el producto, sku = '.$sku);
-            continue;
-        
-        } else {
-           
-            $registros = $datosClientes->registros;
-            // print_r($registros[0]);
-            // die;
-        
-            $imgVisd = $registros[0]->imagenes;
-            // print_r($imgVisd[0]->visd);
-            // die;
-        
-            foreach ($imgVisd as $key => $value) {
-                
-                $images[$key] = [ 
-                    'src' => (string)'http://80.35.251.17/cgi-vel/pruebas/'.$value->visd,
-                ];
-            }
-        
-            $catFamilia = $registros[0]->familia;
-            $familia = [];
-            foreach ($catFamilia as $key1 => $value1) {
-                           
-                $familia[$key1] = $value1;         
-                
-                $data = [
-                    'name' => $registros[0]->nombre,
-                    // Options: simple, grouped, external and variable. Default is simple. SOLO TIENE ESTOS TIPOS 
-                    'type' => 'simple',
-                    'regular_price' =>  (string)$registros[0]->{'tarifa-1'}->precio,
-                    'description' => $registros[0]->catalogo,
-                    'short_description' => $registros[0]->metaDescripcion,
-                    'sku' => (string)$sku,
-                    'dimensions' => [
-            
-                        'length' => (string)$registros[0]-> largo,
-                        'width' => (string)$registros[0]-> ancho,
-                        'height' => (string)$registros[0]-> alto
-                    ],
-                    
-                    'stock_quantity' => $registros[0]->existencias->existencias,
-                    //stock_status Options: instock, outofstock, onbackorder. Default is instock.
-                    //aqui se podria solucionar mirando el stock de vnvm y eligiendo la opcion correcta 
-                    'stock_status' =>'instock',
-                    //Catalog visibility. Options: visible, catalog, search and hidden. Default is visible.
-                    'catalog_visibility' => 'visible',
-                    'sale_price' => (string)$registros[0]->oferta,
-                    //esto es un boolean por defecto false
-                    //'featured' => false,
-                    // 'date_on_sale_from' =>null,
-                    // 'date_on_sale_to' => null,
-                    //para la categoria , la misma tiene que estar creada previamente en WOOCOMERCE y el id es numerico 
-                    'categories' => [
-                        [
-                            'id' => '9'
-                        ],
-                    ],
-                    'images' => [
-                        [
-                            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg'
-                        ],
-                        [
-                            'src' => 'http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg'
-                        ]
-                    ]
-
-                    //'images' => $images
-                ];
-            }
-            $resultCreate = $woocommerce->post('products',  $data);
-        
-            if (!$resultCreate) {
-                echo ("❗Error al actualizar productos \n");
-            } else {
-                print("✔ Productos actualizados correctamente \n <br>");
-                print_r($resultCreate);
-            }
-        }
-      
     }
- 
+
+    foreach ($objDetPed as $key1 => $value1) {
+                
+        $shipping_lines[$key1] = [ 
+            'method_id' => 'flat_rate',
+            'method_title' => 'Flat Rate',
+            'total' => $value1->precio <= 0 ? '0.00' : (string)$value1->precio
+        ];
+    }
+
+    $data = [
+        //string id del metodo de pago
+        'payment_method' => "bacs",
+        'payment_method_title' => 'Direct Bank Transfer',
+        'set_paid' => true,
+        'billing' => [
+            'first_name' => $obj->nombre,
+            'last_name' =>  $obj->nombre,
+            'address_1' => $obj->direccion,
+            'address_2' => $obj->direccion,
+            'city' => $obj->localidad,
+            'state' =>$obj->provincia,
+            'postcode' => $obj->codigoPostal,
+            'country' => $obj->pais->siglas,
+            'email' => $registros->cliente->email,
+            'phone' => '-'
+        ],
+        'shipping' => [
+            'first_name' => $obj->nombre,
+            'last_name' => $obj->nombre,
+            'address_1' => $obj->direccion,
+            'address_2' => $obj->direccion,
+            'city' => $obj->localidad,
+            'state' => $obj->provincia,
+            'postcode' =>$obj->codigoPostal,
+            'country' => $obj->pais->siglas
+        ],
+
+        'line_items' => $line_items,
+
+        'shipping_lines' => $shipping_lines
+    ];
+
+    // print_r($data);
+    // die;
+    
+    $result = $woocommerce->post('orders',  $data);
+    
+    if (!$result) {
+        echo ("❗Error al actualizar productos \n");
+    } else {
+        print("✔ Productos actualizados correctamente \n <br>");
+        print_r($result);
+        die;
+    }
+}
